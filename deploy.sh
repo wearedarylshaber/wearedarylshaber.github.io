50dee8feab416d31ab0eebffbe2ec600e5822e69
Patrick Connelly <patrick@deadlypenguin.com>
2017-03-09 11:16:21 -0500

    Changing some branch structure

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
+SOURCE_BRANCH="source"
+TARGET_BRANCH="master"
 
 function doCompile {
 	gulp dist
